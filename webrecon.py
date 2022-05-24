f8254ba2df4da32234333166479548e4308def2b
mkeck3672 <mike.extra84@gmail.com>
2022-05-23 21:49:46 -0500

    phase 1 step 2
    Need to look into creating a while loop for the read and write.

--- webrecon.py
+++ webrecon.py
@@ -7,3 +7,5 @@ for record in data['records']:
     with open("clean.json", "w") as outfile:
         outfile.write(record['domain'])
     # print(record['domain'])
+# I think i need to change this to a while loop
+# right now I am only outputting the last entry of the file that is being read in.
